fc4b879b62c04e6303eb9bd8a62e7125f6b14af4
1F10230183 <s1F102301831@iniad.org>
2023-12-08 00:13:59 +0900

    CSSmoduleを使用

--- src/contents/moocs.tsx
+++ src/contents/moocs.tsx
@@ -3,6 +3,8 @@ import type { PlasmoCSConfig, PlasmoGetInlineAnchor } from "plasmo"
 import { useEffect, useState } from "react"
 import type { Deadline } from "src/types/moocsTypes"
 
+import styles from "../contents/moocs/index.module.css"
+
 export const config: PlasmoCSConfig = {
   matches: ["https://moocs.iniad.org/*"],
   all_frames: true
@@ -39,22 +41,16 @@ const PlasmoInline = () => {
       const aside = document.querySelector(".sidebar-menu.tree")
       if (aside instanceof HTMLUListElement) {
         const newLi = document.createElement("li")
-        // ここからCSS指定。将来的にはインポートしていきたい
+        // ここからCSS指定。paddingだけCSS.moduleきかなかったので直書き。
+        newLi.className = styles.titleOfHeader
+        newLi.style.padding = " 10px 25px 10px 15px"
         newLi.textContent = "INIAD# CONTENTS"
-        newLi.style.textAlign = "left"
-        newLi.style.backgroundColor = "#1a2226"
-        newLi.style.padding = "10px 25px 10px 15px"
-        newLi.style.color = "#4b646f"
-        newLi.style.fontSize = "12px"
-        newLi.style.listStyleType = "none"
         aside.appendChild(newLi)
 
         const deadlineElement = document.createElement("li")
-        deadlineElement.textContent = "課題締切"
-        deadlineElement.style.textAlign = "left"
-        deadlineElement.style.color = "#b8c7ce"
-        deadlineElement.style.fontSize = ""
+        deadlineElement.className = styles.deadline
         deadlineElement.style.padding = "12px 5px 12px 15px"
+        deadlineElement.textContent = "課題締切"
         aside.append(deadlineElement)
         const deadlineInfo = document.createElement("div")
         deadlineInfo.textContent = `締め切り: ${newData.month}/${newData.day} ${newData.hour}時`
