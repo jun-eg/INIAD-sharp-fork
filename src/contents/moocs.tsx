b93e9247d41624193a6cebd9e0f0d3a2c7ecc626
1F10230183 <s1F102301831@iniad.org>
2023-12-07 15:53:11 +0900

    INIAD# CONTENTSを表示まで

--- src/contents/moocs.tsx
+++ src/contents/moocs.tsx
@@ -1,45 +1,57 @@
 import axios from "axios"
-import type { PlasmoCSConfig } from "plasmo"
-import { useCallback, useEffect, useState } from "react"
-import type { Data } from "src/types/moocsTypes"
+import type { PlasmoCSConfig, PlasmoGetInlineAnchor } from "plasmo"
+import { useEffect, useState } from "react"
 
 export const config: PlasmoCSConfig = {
-  matches: ["https://moocs.iniad.org/*"],
-  all_frames: true
+  matches: ["https://moocs.iniad.org/*"]
 }
-const url = `https://raw.githubusercontent.com/jun-eg/deadline-json-fork/main/data.json`
 
-const FetchDeadLineData = () => {
-  const [data, setData] = useState<Data | null>(null)
+const url = `https://raw.githubusercontent.com/s1f102003425/deadpiro-json/main/data.json`
 
-  const getFiledata = useCallback(async () => {
-    try {
-      const response = await axios.get<Data>(url)
-      setData(response.data)
-    } catch (error) {
-      console.error(error)
-    }
-  }, [])
+// JSONデータを取得する非同期関数
+const fetchFileContent = async () => {
+  const response = await axios.get(url)
+  return response.data
+}
+
+// Plasmoコンテンツスクリプトの設定
+
+// コンテンツを挿入する場所を指定。呼び出さずとも宣言だけで指定した場所に要素を配置できる。
+export const getInlineAnchor: PlasmoGetInlineAnchor = async () => {
+  const aside = document.querySelector(".sidebar-menu.tree") as HTMLUListElement
+  return aside
+}
+export const getShadowHostId = () => "INIAD-sharp"
+
+// PlasmoInlineコンポーネント
+const PlasmoInline = () => {
+  const [dataLoaded, setDataLoaded] = useState(false)
 
   useEffect(() => {
-    getFiledata()
-  }, [getFiledata])
+    if (!dataLoaded) {
+      const fetchData = async () => {
+        const data = await fetchFileContent()
+        console.log(data.cs2.deadline)
+        const aside = document.querySelector(".sidebar-menu.tree")
+        if (aside instanceof HTMLUListElement) {
+          const newLi = document.createElement("li")
+          newLi.textContent = "INIAD# CONTENTS"
+          newLi.style.textAlign = "left"
+          newLi.style.backgroundColor = "#1a2226"
+          newLi.style.padding = "10px 25px 10px 15px"
+          newLi.style.color = "#4b646f"
+          newLi.style.fontSize = "12px"
+          newLi.style.listStyleType = "none"
+          aside.appendChild(newLi)
+        }
+        setDataLoaded(true)
+      }
 
-  if (!data) return <div>GettingData</div>
+      fetchData()
+    }
+  }, [dataLoaded]) // dataLoadedを依存配列に追加
 
-  return (
-    <div>
-      <p>
-        {`${data.year[2023].classes["RW2"].curses["class2"].name}, ${data.year[2023].classes["RW2"].curses["class2"].description},${data.year[2023].classes["RW2"].curses["class2"].deadline.month}/${data.year[2023].classes["RW2"].curses["class2"].deadline.day}/${data.year[2023].classes["RW2"].curses["class2"].deadline.hour}時`}
-      </p>
-      <p>
-        {`${data.year[2023].classes["cs2"].curses["class2"].name}, ${data.year[2023].classes["cs2"].curses["class2"].description},${data.year[2023].classes["cs2"].curses["class2"].deadline.month}/${data.year[2023].classes["cs2"].curses["class2"].deadline.day}/${data.year[2023].classes["cs2"].curses["class2"].deadline.hour}時`}
-      </p>
-      <p>
-        {`${data.year[2023].classes["情報連携学概論"].curses["none"].name}, ${data.year[2023].classes["情報連携学概論"].curses["none"].description},${data.year[2023].classes["情報連携学概論"].curses["none"].deadline.month}/${data.year[2023].classes["情報連携学概論"].curses["none"].deadline.day}/${data.year[2023].classes["情報連携学概論"].curses["none"].deadline.hour}時`}
-      </p>
-    </div>
-  )
+  return null
 }
 
-export default FetchDeadLineData
+export default PlasmoInline
