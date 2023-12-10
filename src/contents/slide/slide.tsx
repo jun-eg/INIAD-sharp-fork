86d7ca476826a46a8caa73f3e018f3a3029a7da4
s1f10230014 <s1f102300144@iniad.org>
2023-12-11 01:07:43 +0900

    add:testData slide.tsx

--- src/contents/slide/slide.tsx
+++ src/contents/slide/slide.tsx
@@ -2,6 +2,22 @@ import axios from "axios"
 import { useCallback, useEffect, useState } from "react"
 import type { Data } from "src/types/slideTypes"
 
+const testData = [
+  {
+    name: "2023",
+    id: 1,
+    children: [
+      { name: "cs1", id: 2 },
+      { name: "cs2", id: 3 }
+    ]
+  }
+]
+$("#tree1").tree({
+  data: testData,
+  autoOpen: true,
+  drabAndDrop: true
+})
+
 const url = `https://raw.githubusercontent.com/jun-eg/deadline-json-fork/main/data.json`
 
 const FetchSlideData = () => {
