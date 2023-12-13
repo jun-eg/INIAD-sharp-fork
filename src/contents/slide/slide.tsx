ec74a63159e9eb6c457e658d680097bae6fcacaf
s1f10230014 <s1f102300144@iniad.org>
2023-12-13 19:54:43 +0900

    fix:slide.tsxの中身を戻した

--- src/contents/slide/slide.tsx
+++ src/contents/slide/slide.tsx
@@ -1,60 +1,28 @@
 import axios from "axios"
-import $ from "jquery"
 import { useCallback, useEffect, useState } from "react"
 import type { Data } from "src/types/slideTypes"
 
-import "jqtree"
-
-type TreeNode = {
-  name: string
-  id: number
-  children?: TreeNode[] // `children` はオプショナルで、TreeNode型の配列
-}
-
-const testData = [
-  {
-    name: "2023",
-    id: 1,
-    children: [
-      { name: "cs1", id: 2 },
-      { name: "cs2", id: 3 }
-    ]
-  }
-]
+const url = `https://raw.githubusercontent.com/jun-eg/deadline-json-fork/main/data.json`
 
 const FetchSlideData = () => {
-  const [data, setData] = useState<TreeNode[]>()
+  const [data, setData] = useState<Data | null>(null)
 
-  // const getFiledata = useCallback(async () => {
-  //   try {
-  //     const response = await axios.get<Data>(url)
-  //     setData(response.data)
-  //   } catch (error) {
-  //     console.error(error)
-  //   }
-  // }, [])
-
-  const settree = () => {
-    $("#tree1").tree({
-      data: testData,
-      autoOpen: true,
-      dragAndDrop: true
-    })
-
-    setData(testData)
-  }
+  const getFiledata = useCallback(async () => {
+    try {
+      const response = await axios.get<Data>(url)
+      setData(response.data)
+    } catch (error) {
+      console.error(error)
+    }
+  }, [])
 
   useEffect(() => {
-    settree()
-  }, [])
+    getFiledata()
+  }, [getFiledata])
 
   if (!data) return <div>GettingData</div>
 
-  return (
-    <>
-      <div id="tree1" />
-    </>
-  )
+  return <div />
 }
 
 export default FetchSlideData
